79d616e688909810d181dd89a4740f7ed9887de7
Aman prajapati <96925529+puroaman6606@users.noreply.github.com>
2025-04-11 21:07:42 +0530

    Update header.tsx

--- components/header.tsx
+++ components/header.tsx
@@ -4,10 +4,10 @@ export default function Header() {
   return (
     <WavyBackground className="max-w-4xl mx-auto pb-20">
       <p className="text-4xl md:text-4xl lg:text-7xl text-white font-bold inter-var text-center">
-        Smart Check, AI Smart Contract Auditor
+        Smart Check, AI Python based Smart Contract Auditor
       </p>
       <p className="text-base md:text-2xl mt-4 text-white font-normal inter-var text-center">
-        Leverage the power of AI to audit your smart contracts
+        Leverage the power of AI to audit your Python based Smart Contracts
       </p>
     </WavyBackground>
   );
